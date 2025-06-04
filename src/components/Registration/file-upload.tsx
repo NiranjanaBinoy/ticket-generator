7882b4c9ec861d6389b11bfc3a1ab0da7d2481cf
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2025-06-04 13:11:26 -0400

    adding avatar size validation

--- src/components/Registration/file-upload.tsx
+++ src/components/Registration/file-upload.tsx
@@ -2,21 +2,46 @@ import { useDropzone } from "react-dropzone";
 import uploadIcon from '../../assets/icon-upload.svg';
 import { FILE_UPLOAD } from "../../constants/constants";
 import { useTicketContext } from "../../hooks/useTicketContext";
+import HintText from "./hint-text";
 
 const FileUpload = () => {
     const {avatar, handleAvatar} = useTicketContext();
 
-    const { getRootProps, getInputProps } = useDropzone({
+    const maxSize = 500000; // 500KB
+
+    function fileSizeValidator(file: File) {
+    if(file.size > maxSize) {
+        handleAvatar('');
+        return {
+            code: "file-too-large",
+            message: "File size exceeds 500KB"
+        }
+    }
+
+    return null
+    }
+    const { fileRejections, getRootProps, getInputProps } = useDropzone({
+        accept: {
+            'image/jpeg': ['.jpeg', '.png']
+        },
         onDrop: (acceptedFiles) => {
             handleAvatar(URL.createObjectURL(acceptedFiles[0]));
-        }
+        },
+        validator: fileSizeValidator,
+        maxFiles: 1,
     });
+
+    const fileRejectionItems = fileRejections.map(({ errors }) => (
+        <HintText message={errors[0].message}/>
+    ));
+
     return(
         <div className="file-upload" {...getRootProps()}>
             <div className="upload-icon"><img src={uploadIcon} /></div>
             <input {... getInputProps()} />
             <p>{FILE_UPLOAD}</p>
             {avatar && <img src={avatar} />}
+            {fileRejectionItems ? fileRejectionItems : null}
         </div>
     )
 }
